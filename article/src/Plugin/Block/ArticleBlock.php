e80660408fa953a5a4255aee2755b4ddf9967bbe
xaiwant <jaywanttopno@yahoo.com>
2016-01-06 11:36:33 +0530

    custom block plugin

---
+++ article/src/Plugin/Block/ArticleBlock.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\xai\Plugin\Block\XaiBlock.
+ */
+
+namespace Drupal\article\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+
+/**
+ * Provides a 'article' block.
+ *
+ * @Block(
+ *   id = "article_block",
+ *   admin_label = @Translation("Article block"),
+ *   category = @Translation("Custom article block example")
+ * )
+ */
+class ArticleBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => 'This block list the article.',
+    );
+  }
+}
